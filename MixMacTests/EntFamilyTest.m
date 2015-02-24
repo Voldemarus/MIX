c8f93144fe929d5ea4a007c6e9b314947f9a0c2a
Vladimir Vodolazkiy <Vladimir.Vodolazkiy@asteros.ru>
2015-02-24 14:54:58 +0300

    ENNA and ENNX tests added

--- MixMacTests/EntFamilyTest.m
+++ MixMacTests/EntFamilyTest.m
@@ -26,8 +26,6 @@
 		[cpu setMemoryWord:command forCellIndex:TEST_PC];
 		
 		cpu.A = [self mixWordFromInteger:0];			// initial value to accumulator
-		
-		[self printMemoryCell:[cpu memoryWordForCellIndex:TEST_PC]];
 		cpu.PC = TEST_PC;
 		
 		long accumulator = [self integerFromMIXWORD:cpu.A];
@@ -55,8 +53,33 @@
 	XCTAssertFalse(cpu.A.sign, @"sign bit in accumulator should be ON");
 	long result = [self integerFromMIXWORD:cpu.A];
 	XCTAssertEqual(result,0, @"Accumulator should contain zero");
-	
 }
+
+
+- (void) testENNA
+{
+	// Initial test - simple case: load to accumulator constants
+	long data[10] = { 120, 2345, -343, -4067, 1111, 1152, 3433, -2330, -221, -667 };
+	for (int i = 0; i < 10; i++) {
+		// use default modifier for these tests
+		MIXWORD command = [self mixCommandForMnemonic:@"ENNA" withAddress:data[i] index:0 andModifier:MIX_F_NOTDEFINED];
+		
+		[cpu setMemoryWord:command forCellIndex:TEST_PC];
+		
+		cpu.A = [self mixWordFromInteger:0];			// initial value to accumulator
+		
+		cpu.PC = TEST_PC;
+		
+		long accumulator = [self integerFromMIXWORD:cpu.A];
+		XCTAssertEqual(accumulator, 0, @"accumulator should be properly cleared");
+		
+		[cpu executeCurrentOperation];
+		
+		accumulator = -[self integerFromMIXWORD:cpu.A];
+		XCTAssertEqual(accumulator, data[i], @"Data shpuld be loaded into accumulator properly");
+	}
+}
+
 
 - (void) testENTX
 {
@@ -103,8 +126,57 @@
 	XCTAssertFalse(cpu.X.sign, @"sign bit in X should be ON");
 	long result = [self integerFromMIXWORD:cpu.X];
 	XCTAssertEqual(result,0, @"X should contain zero");
-	
 }
+
+
+- (void) testENNX
+{
+	// Initial test - simple case: load to accumulator constants
+	long data[10] = { 120, 2345, -343, -4067, 1111, 1152, 3433, -2330, -221, -667 };
+	for (int i = 0; i < 10; i++) {
+		// use default modifier for these tests
+		MIXWORD command = [self mixCommandForMnemonic:@"ENNX" withAddress:data[i] index:0 andModifier:MIX_F_NOTDEFINED];
+		
+		[self printMemoryCell:command];
+		
+		[cpu setMemoryWord:command forCellIndex:TEST_PC];
+		
+		cpu.X = [self mixWordFromInteger:0];			// initial value to accumulator
+		
+		[self printMemoryCell:[cpu memoryWordForCellIndex:TEST_PC]];
+		cpu.PC = TEST_PC;
+		
+		
+		long accumulator = [self integerFromMIXWORD:cpu.X];
+		XCTAssertEqual(accumulator, 0, @"X register should be properly cleared");
+		
+		[cpu executeCurrentOperation];
+		
+		[self printMemoryCell:cpu.X];
+		
+		accumulator = -[self integerFromMIXWORD:cpu.X];
+		XCTAssertEqual(accumulator, data[i], @"Data shpuld be loaded into X register properly");
+	}
+	// special case: if effective address is equal to 0, sign is derived from the command sign byte
+	//  ENTX -100, 4
+	
+	MIXWORD command = [self mixCommandForMnemonic:@"ENTX" withAddress:-100 index:4 andModifier:MIX_F_NOTDEFINED];
+	[cpu storeOffset:100 inIndexRegister:4];
+	[cpu setMemoryWord:command forCellIndex:TEST_PC];
+	
+	cpu.PC = TEST_PC;
+	
+	cpu.X = [self mixWordFromInteger:0];			// initial value to accumulator
+	XCTAssertFalse(cpu.X.sign, @"sign bit in X should be off");
+	
+	[cpu executeCurrentOperation];
+	
+	XCTAssertFalse(cpu.X.sign, @"sign bit in X should be ON");
+	long result = [self integerFromMIXWORD:cpu.X];
+	XCTAssertEqual(result,0, @"X should contain zero");
+}
+
+
 
 - (void) testENTI
 {
